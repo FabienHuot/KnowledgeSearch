b2dff7c67e67a70e093856c235584afc4ef5b313
HUOT Fabien (Consulting for Edenred) <fabien.huot@consulting-for.edenred.com>
2019-07-18 10:58:01 +0200

    Modifiying setupScratchOrg

--- setupScratchOrg.cmd
+++ setupScratchOrg.cmd
@@ -3,6 +3,9 @@ echo '##### CREATING SCRATCH ORG #####'
 call sfdx force:org:create -f config/project-scratch-def.json -a KnowledgeSearch -s
 echo '##### PUSHING METADATA #####'
 call sfdx force:source:push -u KnowledgeSearch
+echo '##### KNOWLEDGE PERMISSIONSET ASSIGNMENT #####'
 call sfdx force:user:permset:assign -n KnowledgeUser -u KnowledgeSearch
+echo '##### CREATE KNOWLEDGE USER #####'
+sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
 echo '##### OPENING SCRATCH ORG #####'
 call sfdx force:org:open
